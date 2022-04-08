335c42df08478fd88b5697be85d74f6624d84de1
MalindaH <malindahlh.mh@gmail.com>
2022-04-08 11:14:34 -0400

    update bio

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -9,9 +9,9 @@ export default function AboutMe() {
             <img className='col-3' src={hello} alt="my-icon"/>
             <div className='col-9 mt-auto mb-auto'>
                 <p>
-                Hi there! My name is Linhui Huang and I also go by Malinda. I am a third- year (U2) student at McGill University, 
+                Hi there! My name is Malinda Huang. I am in my last year (U3) at McGill University, 
                 studying Computer Science and Biology Joint Honours Program.<br />
-                I have 3+ years of experience building programs, websites, and games, with proficient knowledge in computer 
+                I have 5+ years of experience building programs, websites, and games, with proficient knowledge in computer 
                 science, statistics, machine learning, and biology.
                 </p>
             </div>
